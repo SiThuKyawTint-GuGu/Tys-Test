9061c0878c85bf110e579781ddd31f3276ed4226
Gu Gu <sithukyawtint006@gmail.com>
2023-04-24 21:59:50 +0630

    first

---
+++ src/sandbox.ts
@@ -0,0 +1,30 @@
+let number = [1, 2, 3, 4,'sithu',false]
+
+number.push(false)
+number[2] = 'sr'
+
+// let info = {
+//     name: 'Si Thu',
+//     age: 19,
+//     job:'developer'
+// }
+
+// info.age = 22
+// info.job = 'sithu'
+
+
+// let info  : {
+//     name: String,
+//     age: Number,
+//     salry : Number,
+// }
+
+// info = {
+//     name: 'sithu',
+//     age: 22,
+//    salry : 200000
+// }
+
+let x: Object[]
+
+
