64056b36e394dad3417c76668a85805e3dd06d13
Gu Gu <sithukyawtint006@gmail.com>
2023-04-25 21:22:22 +0630

    add field

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,22 +1,57 @@
 "use strict";
-let number = [1, 2, 3, 4, 'sithu', false];
-number.push(false);
-number[2] = 'sr';
+// let number = [1, 2, 3, 4,'sithu',true]
+// number.push(false)
+// number[2] = 11
 // let info = {
 //     name: 'Si Thu',
 //     age: 19,
 //     job:'developer'
 // }
-// info.age = 22
+// info.age = 18
 // info.job = 'sithu'
 // let info  : {
 //     name: String,
 //     age: Number,
-//     salry : Number,
+//     salry: Number,
+//     job:String
 // }
 // info = {
 //     name: 'sithu',
 //     age: 22,
-//    salry : 200000
+//     salry: 350000,
+//     job:"developer"
 // }
-let x;
+// Dynamic Types
+// let mixed: any[] = [];
+// mixed.push('hekki')
+// mixed.push(11)
+// mixed.push(false)
+// console.log(mixed)
+// let ninja: {
+//     name: any,
+//     age : any,
+// }
+// ninja = {
+//     name: [1,2,3,4,5],
+//     age: false
+// }
+// console.log(ninja)
+// Function Basic
+let greet;
+// greet = (x : number,y:number) => {
+//     return x + y
+// }
+// console.log(greet(1,2))
+// greet = (a: number, b: number, c: string) => {
+//     return a + b + c
+// }
+// console.log(greet(1,2,'ss'))
+// type stornum = string | number
+// const z = (a: number, b: number, c: stornum = 10) : void => {
+//    console.log(a,b,c)
+// }
+// console.log(z(1,2,4))
+// const ff = (a: number, b: number, c?: string | number): number => {
+//     return a + b 
+//  }
+// console.log(ff(1,2))
